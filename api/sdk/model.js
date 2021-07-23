9961e4ef82b3fd1dba62bcb00ba307d2085e0c35
limid404 <85405089+limid404@users.noreply.github.com>
2021-07-23 19:07:33 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,24 +1,30 @@
 const tf = require('@tensorflow/tfjs-node');
 
 function normalized(data){ // i & r
-    x1 = (data[0] - 42.773) / 10.33017
-    x2 = (data[1] - 29.9412) / 8.936247
-    x3 = (data[2] - 94.8964) / 8.887377
-
-    return [x1, x2, x3]
+    x1 = (data[0] - 83) / 8.48528137423857
+    x2 = (data[1] - 27) / 1.4142135623731
+    x3 = (data[2] - 26) / 4.24264068711928
+    x4 = (data[3] - 544) / 11.3137084989848
+    
+    
+    return [x1, x2, x3, x4]
 }
 
 function denormalized(data){
-    y1 = (data[0] * 16.08198) + 32.2718
-    y2 = (data[1] * 8.918185) + 39.959
-    y3 = (data[2] * 11.79956) + 69.739
-
-    return [y1, y2, y3]
+    y1 = (data[0] * 57873) + 6577.50727859727
+    y2 = (data[1] * 10160) + 342.239682094289
+    y1 = (data[2] * 91.2219251595535) + 6.09267924394864
+    y2 = (data[3] * 2235) + 111.722871427475
+    y1 = (data[4] * 2140) + 131.521861300698
+    y2 = (data[5] * 705) + 151.320851173921
+    
+    
+    return [y1, y2, y3, y4, y5, y6]
 }
 
 
 async function predict(data){
-    let in_dim = 3;
+    let in_dim = 4;
     
     data = normalized(data);
     shape = [1, in_dim];
@@ -27,7 +33,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Afaizin-bitt/UAS_JST/main/public/Model5/model.json';
+        const path = 'https://raw.githubusercontent.com/limid404/jstremidi/main/public/Model5/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
